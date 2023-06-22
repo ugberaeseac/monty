1adccd0c6ace1adcd99f6b5bf4970aeda0846b1a
SouaOui <soumiaouzat@gmail.com>
2023-06-22 12:11:26 -0700

    help to implement the push command free the stack

---
+++ helpers.c
@@ -0,0 +1,50 @@
+#include "monty.h"
+/**
+ * helper_push - function that add at the begining of the list
+ * @head: pointer to the top of the stack
+ * @number: data of the element to be added to the stack
+ */
+void helper_push(stack_t **head, unsigned int number)
+{
+	stack_t *new_node;
+
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+		return;
+	new_node->n = number;
+	new_node->next = *head;
+	if (*head != NULL)
+		(*head)->prev = new_node;
+	*head = new_node;
+}
+/**
+ * _isdigit -  a function that checks for a digit 0-9
+ * @c: input
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
+
+/**
+ * free_dlistint - function that free the whole list
+ * @head: pointer to the first node in the list
+ * Return: Nothing
+ */
+void free_dlistint(stack_t *head)
+{
+	stack_t *current;
+
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current);
+	}
+}
+
