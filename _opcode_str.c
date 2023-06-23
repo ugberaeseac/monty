6844e25d597e4fb77bee30f0c0a62ff4a030ba59
UGBERAESE Amowie Charles <ugberaeseac@gmail.com>
2023-06-23 05:10:55 -0700

    debugged pstr opcode to not print element not in the ascii table

--- _opcode_str.c
+++ _opcode_str.c
@@ -43,7 +43,7 @@ void _pstr(stack_t **stack, unsigned int line_number)
 	(void)line_number;
 	temp = (*stack);
 	if ((*stack) == NULL || stack == NULL)
-		putchar('\n');
+		printf("\n");
 
 	while ((temp != NULL) && (temp->n != 0) &&
 			(temp->n <= 127) && (temp->n >= 0))
