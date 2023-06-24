e66e9705b97fed2c41998c163352a29b5c884509
UGBERAESE Amowie Charles <ugberaeseac@gmail.com>
2023-06-24 01:26:22 -0700

    added the _nop function prototype to header file

--- monty.h
+++ monty.h
@@ -61,6 +61,7 @@ void _div(stack_t **stack, unsigned int line_number);
 void _mod(stack_t **stack, unsigned int line_number);
 void _pchar(stack_t **stack, unsigned int line_number);
 void _pstr(stack_t **stack, unsigned int line_number);
+void _nop(stack_t **stack, unsigned int line_number);
 
 #endif
 
